acc07c1c526f8307c2b5d1052b0a3005af1246ff
Tejas1 <tejasdeshpande2007@gmail.com>
2015-07-17 23:28:28 +0530

    Ospinet Code

--- com.ospinet.SplashScreen/src/com/ospinet/app/ContactsMainActivity.java
+++ com.ospinet.SplashScreen/src/com/ospinet/app/ContactsMainActivity.java
@@ -7,10 +7,21 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.ospinet.app.AlphabetListAdapter.Item;
 import com.ospinet.app.AlphabetListAdapter.Row;
 import com.ospinet.app.AlphabetListAdapter.Section;
+
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -21,6 +32,7 @@ import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContactsMainActivity extends ListActivity {
 
@@ -32,7 +44,8 @@ public class ContactsMainActivity extends ListActivity {
     private static float sideIndexX;
     private static float sideIndexY;
     private int indexListSize;
-
+    ProgressDialog dialog;
+    ArrayList<contact> arrcontacts;
     class SideIndexGestureListener extends GestureDetector.SimpleOnGestureListener {
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
@@ -53,61 +66,9 @@ public class ContactsMainActivity extends ListActivity {
         setContentView(R.layout.list_alphabet);
 
         mGestureDetector = new GestureDetector(this, new SideIndexGestureListener());
-
-        List<String> countries = populateCountries();
-        Collections.sort(countries);
-
-        List<Row> rows = new ArrayList<Row>();
-        int start = 0;
-        int end = 0;
-        String previousLetter = null;
-        Object[] tmpIndexItem = null;
-        Pattern numberPattern = Pattern.compile("[0-9]");
-
-        for (String country : countries) {
-            String firstLetter = country.substring(0, 1);
-
-            // Group numbers together in the scroller
-            if (numberPattern.matcher(firstLetter).matches()) {
-                firstLetter = "#";
-            }
-
-            // If we've changed to a new letter, add the previous letter to the alphabet scroller
-            if (previousLetter != null && !firstLetter.equals(previousLetter)) {
-                end = rows.size() - 1;
-                tmpIndexItem = new Object[3];
-                tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
-                tmpIndexItem[1] = start;
-                tmpIndexItem[2] = end;
-                alphabet.add(tmpIndexItem);
-
-                start = end + 1;
-            }
-
-            // Check if we need to add a header row
-            if (!firstLetter.equals(previousLetter)) {
-                rows.add(new Section(firstLetter));
-                sections.put(firstLetter, start);
-            }
-
-            // Add the country to the list
-            rows.add(new Item(country));
-            previousLetter = firstLetter;
-        }
-
-        if (previousLetter != null) {
-            // Save the last letter
-            tmpIndexItem = new Object[3];
-            tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
-            tmpIndexItem[1] = start;
-            tmpIndexItem[2] = rows.size() - 1;
-            alphabet.add(tmpIndexItem);
-        }
-
-        adapter.setRows(rows);
-        setListAdapter(adapter);
-
-        updateList();
+        dialog = new ProgressDialog(ContactsMainActivity.this);
+        arrcontacts = new ArrayList<contact>();
+        new Loadcontacts().execute();
     }
 
     @Override
@@ -189,56 +150,137 @@ public class ContactsMainActivity extends ListActivity {
         }
     }
 
-    private List<String> populateCountries() {
-        List<String> countries = new ArrayList<String>();
-        countries.add("Afghanistan");
-        countries.add("Albania");
-        countries.add("Bahrain");
-        countries.add("Bangladesh");
-        countries.add("Cambodia");
-        countries.add("Cameroon");
-        countries.add("Denmark");
-        countries.add("Djibouti");
-        countries.add("East Timor");
-        countries.add("Ecuador");
-        countries.add("Fiji");
-        countries.add("Finland");
-        countries.add("Gabon");
-        countries.add("Georgia");
-        countries.add("Haiti");
-        countries.add("Holy See");
-        countries.add("Iceland");
-        countries.add("India");
-        countries.add("Jamaica");
-        countries.add("Japan");
-        countries.add("Kazakhstan");
-        countries.add("Kenya");
-        countries.add("Laos");
-        countries.add("Latvia");
-        countries.add("Macau");
-        countries.add("Macedonia");
-        countries.add("Namibia");
-        countries.add("Nauru");
-        countries.add("Oman");
-        countries.add("Pakistan");
-        countries.add("Palau");
-        countries.add("Qatar");
-        countries.add("Romania");
-        countries.add("Russia");
-        countries.add("Saint Kitts and Nevis");
-        countries.add("Saint Lucia");
-        countries.add("Taiwan");
-        countries.add("Tajikistan");
-        countries.add("Uganda");
-        countries.add("Ukraine");
-        countries.add("Vanuatu");
-        countries.add("Venezuela");
-        countries.add("Yemen");
-        countries.add("Zambia");
-        countries.add("Zimbabwe");
-        countries.add("0");
-        countries.add("2");
-        countries.add("9");
-        return countries;
+    public class Loadcontacts extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please Wait..");
+            dialog.show();
+            dialog.setCancelable(false);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            String retstring = "";
+            try {
+                ArrayList<NameValuePair> allcontacts = new ArrayList<NameValuePair>();
+                SharedPreferences myPrefs = ContactsMainActivity.this
+                        .getSharedPreferences("remember", Context.MODE_PRIVATE);
+                String userId = myPrefs.getString("userid", null);
+                allcontacts.add(new BasicNameValuePair("user_id",userId));
+                String response = CustomHttpClient
+                        .executeHttpPost("http://ospinet.com/app_ws/android_app_fun/get_contacts",
+                                allcontacts);
+                retstring = response.toString();
+            } catch (Exception io) {
+
+            }
+            return retstring;
+
+        }
+
+        @Override
+        protected void onPostExecute(String retstring) {
+            if (dialog.isShowing())
+                dialog.dismiss();
+            JSONObject jsonResponse = null;
+            try {
+                String Friend_id="";
+                String First_Name = "";
+                String Last_Name ="";
+                String Email ="";
+                jsonResponse = new JSONObject(retstring);
+                int flag=0;
+                JSONArray jsonMainNode = jsonResponse
+                        .optJSONArray("result");
+                if(jsonMainNode!=null)
+                {
+                    for (int i = 0; i < jsonMainNode.length(); i++) {
+
+                        JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                        Friend_id = jsonChildNode.optString("friend_id");
+                        First_Name = jsonChildNode.optString("fname");
+                        Last_Name = jsonChildNode.optString("lname");
+                        Email =  jsonChildNode.optString("email");
+                        contact r = new contact();
+                        r.setfriend_id(Friend_id);
+                        r.setFname(First_Name);
+                        r.setLname(Last_Name);
+                        r.setemail(Email);
+                        arrcontacts.add(r);
+                        flag=1;
+                    }
+                }
+                else
+                {
+                    Toast.makeText(ContactsMainActivity.this, "No Contact Found", Toast.LENGTH_LONG).show();
+                }
+
+                List<String> lstContactName = new ArrayList<String>();
+                for (contact contact : arrcontacts) {
+                    lstContactName.add(contact.getFname() + contact.getLname());
+                }
+
+                Collections.sort(lstContactName);
+
+                List<Row> rows = new ArrayList<Row>();
+                int start = 0;
+                int end = 0;
+                String previousLetter = null;
+                Object[] tmpIndexItem = null;
+                Pattern numberPattern = Pattern.compile("[0-9]");
+
+                for (String contact : lstContactName) {
+                    String firstLetter = contact.substring(0, 1);
+
+                    // Group numbers together in the scroller
+                    if (numberPattern.matcher(firstLetter).matches()) {
+                        firstLetter = "#";
+                    }
+
+                    // If we've changed to a new letter, add the previous letter to the alphabet scroller
+                    if (previousLetter != null && !firstLetter.equals(previousLetter)) {
+                        end = rows.size() - 1;
+                        tmpIndexItem = new Object[3];
+                        tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
+                        tmpIndexItem[1] = start;
+                        tmpIndexItem[2] = end;
+                        alphabet.add(tmpIndexItem);
+
+                        start = end + 1;
+                    }
+
+                    // Check if we need to add a header row
+                    if (!firstLetter.equals(previousLetter)) {
+                        rows.add(new Section(firstLetter));
+                        sections.put(firstLetter, start);
+                    }
+
+                    // Add the country to the list
+                    rows.add(new Item(contact));
+                    previousLetter = firstLetter;
+                }
+
+                if (previousLetter != null) {
+                    // Save the last letter
+                    tmpIndexItem = new Object[3];
+                    tmpIndexItem[0] = previousLetter.toUpperCase(Locale.UK);
+                    tmpIndexItem[1] = start;
+                    tmpIndexItem[2] = rows.size() - 1;
+                    alphabet.add(tmpIndexItem);
+                }
+
+                adapter.setRows(rows);
+                setListAdapter(adapter);
+
+                updateList();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
+
 }
